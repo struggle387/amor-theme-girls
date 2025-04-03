698118a079aef2ae605e9ea2628b28ff39a4c32f
coffee4261 <coffee4261@tutamail.com>
2025-04-02 22:04:09 -0400

    adding sudo check and mate checkmark

--- asuka_suit/install.sh
+++ asuka_suit/install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Ensure the script is run with root privileges
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root. Please use sudo."
+  exit 1
+fi
+
 # Copy the asuka_suitrc file to the /usr/share/amor/ directory
 cp asuka_suitrc /usr/share/amor/
 
 # Create the directory for asuka_suit images if it doesn't exist
 mkdir -p /usr/share/amor/pics/animated/asuka_suit/
 
-# Copy the suit1.pbg and suit2.png files to the asuka_suit directory
+# Copy the suit1.png and suit2.png files to the asuka_suit directory
 cp suit1.png suit2.png /usr/share/amor/pics/animated/asuka_suit/
 
 # Copy the asuka_suit_preview.png file to the preview directory
@@ -14,4 +20,3 @@ cp asuka_suit_preview.png /usr/share/amor/pics/preview/
 
 # Output a success message
 echo "Files copied successfully!"
-
