aa6168c573c7a2c4f8a3f0721ed3cd821e77de90
coffee4261 <coffee4261@tutamail.com>
2025-04-02 21:40:36 -0400

    add install.sh

--- asuka_suit/install.sh
+++ asuka_suit/install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Copy the asuka_suitrc file to the /usr/share/amor/ directory
+cp asuka_suitrc /usr/share/amor/
+
+# Create the directory for asuka_suit images if it doesn't exist
+mkdir -p /usr/share/amor/pics/animated/asuka_suit/
+
+# Copy the suit1.pbg and suit2.png files to the asuka_suit directory
+cp suit1.pbg suit2.png /usr/share/amor/pics/animated/asuka_suit/
+
+# Copy the asuka_suit_preview.png file to the preview directory
+cp asuka_suit_preview.png /usr/share/amor/pics/preview/
+
+# Output a success message
+echo "Files copied successfully!"
 
