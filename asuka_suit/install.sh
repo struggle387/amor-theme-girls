83dd0917ccf9acb3a387fb749be5755eac245097
coffee4261 <coffee4261@tutamail.com>
2025-04-02 21:53:16 -0400

    fixed mispelling

--- asuka_suit/install.sh
+++ asuka_suit/install.sh
@@ -7,7 +7,7 @@ cp asuka_suitrc /usr/share/amor/
 mkdir -p /usr/share/amor/pics/animated/asuka_suit/
 
 # Copy the suit1.pbg and suit2.png files to the asuka_suit directory
-cp suit1.pbg suit2.png /usr/share/amor/pics/animated/asuka_suit/
+cp suit1.png suit2.png /usr/share/amor/pics/animated/asuka_suit/
 
 # Copy the asuka_suit_preview.png file to the preview directory
 cp asuka_suit_preview.png /usr/share/amor/pics/preview/
